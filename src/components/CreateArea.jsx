c7f0a56e83968adfc39d1edc5b945bfcc9fa034b
jeetganatra <jeetganatra138@gmail.com>
2021-02-04 01:15:28 +0530

    Completed by improving certain UI elements

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,11 @@
 import React, {useState} from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
+
+  const [isExpanded, setExpanded]=useState(false);
 
   const [note,setNote]=useState({
     title:"",
@@ -26,17 +31,30 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function expand(){
+    setExpanded(true);
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
+      <form className="create-note">
+
+        {isExpanded && <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />}
+
         <textarea 
+          onClick={expand}
           name="content" 
           value={note.content} 
           placeholder="Take a note..." 
           onChange={handleChange}
-          rows="3" />
-        <button onClick={submitNote}> Add</button>
+          rows={isExpanded ?3:1} />
+
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+          <AddIcon />
+        </Fab>
+        </Zoom>
+        
       </form>
     </div>
   );
